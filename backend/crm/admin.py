5f1ce541a2519c230ebdb40c8433b6ac2c12dfc7
kaynye <kemuel.kany@live.fr>
2025-12-16 15:34:33 +0100

    Add backoffice

--- backend/crm/admin.py
+++ backend/crm/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Company, Contact, Contract, Meeting
+from .models import Company, Contact, Contract, Meeting, Document, MeetingTemplate, SharedLink
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(Contact)
 admin.site.register(Contract)
 admin.site.register(Meeting)
+admin.site.register(Document)
+admin.site.register(MeetingTemplate)
+admin.site.register(SharedLink)
