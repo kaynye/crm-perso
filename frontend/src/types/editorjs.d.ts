b3094197c58101fda505c317ffb0c669e8176bad
kaynye <kemuel.kany@live.fr>
2025-12-16 14:40:04 +0100

    fix editor type

---
+++ frontend/src/types/editorjs.d.ts
@@ -0,0 +1,8 @@
+declare module '@editorjs/checklist';
+declare module '@editorjs/delimiter';
+declare module '@editorjs/table';
+declare module '@editorjs/list';
+declare module '@editorjs/header';
+declare module '@editorjs/quote';
+declare module '@editorjs/marker';
+declare module '@editorjs/inline-code';
